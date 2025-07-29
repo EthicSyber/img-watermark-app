2e1b84c7381f3a1df684ee2e7a627080996246dd
Christopher Moore <coffeecipher@Christophers-MacBook-Pro-2.local>
2025-07-29 15:04:02 -0700

    Add files

--- TODOs/todo.py
+++ TODOs/todo.py
@@ -1,11 +1,11 @@
 # TODO'S (INCOMPLETE)
 
-# TODO: Research Pillow for image handling and watermarking
+# TODO: Research Pillow for image handling and watermarking; url: https://pillow.readthedocs.io/en/stable/
 # * TODO: Create a function to handle for image processing
 # * TODO: Create a function to handle watermarking an image
 # TODO: Create a ImageWatermark class to incorporate the above functions
 
-# TODO: Research Tkinter for GUI app creation
+# TODO: Research Tkinter for GUI app creation; url: https://docs.python.org/3/library/tkinter.html, https://tkdocs.com
 # * TODO: Create a GUI for the Watermark Application to be wrapped
 # * TODO: Design an background for the application and interface (buttons, backgrounds, etc.)
 # * TODO: Wrap the application to be accessible by executing a binary, executble, etc..
